66224f99b6ab6f3ea522b8f087cf8feaa9f29848
z64 <zachnowicki@gmail.com>
2016-12-15 01:44:39 -0500

    add client.cr example

---
+++ examples/client.cr
@@ -0,0 +1,32 @@
+require "../src/amnesia"
+
+if ARGV.empty?
+  puts "Usage:"
+  puts " amnesia -d                       List available domains"
+  puts " amnesia email@domain [interval]  Bind to an email and poll it at interval"
+  exit
+end
+
+if ARGV[0] == "-d"
+  puts Amnesia::EmailAddress.domains.join "\n"
+  exit
+end
+
+begin
+  client = Amnesia::Client.new ARGV[0]
+rescue e
+  puts "Could not bind client! #{e}"
+  exit
+end
+
+poll_interval = ARGV[1]? || 5
+
+puts "Press CTRL+C to terminate."
+
+loop do
+  client.poll! do |e|
+    puts "from: #{e.from}"
+    puts "subject: #{e.subject}"
+    puts "body: #{e.text}"
+  end
+end
