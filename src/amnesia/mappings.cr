c51e00f9dddb414550a8c656c77bbf272f40ff44
z64 <zachnowicki@gmail.com>
2016-12-14 22:45:24 -0500

    add Email mapping

--- src/amnesia/mappings.cr
+++ src/amnesia/mappings.cr
@@ -1,4 +1,23 @@
 require "json"
 
 module Amnesia
+  # Email object from temp-mail
+  class Email
+    JSON.mapping({
+      id: { type: String, key: "mail_id" },
+      address_id: { type: String, key: "mail_address_id" },
+      from: { type: String, key: "mail_from" },
+      subject: { type: String, key: "mail_subject" },
+      preview: { type: String, key: "mail_preview" },
+      text_only: { type: String, key: "mail_text_only" },
+      text: { type: String, key: "mail_text" },
+      html: { type: String, nilable: true, key: "mail_html" },
+      timestamp: { type: Float32, key: "mail_timestamp" }
+    }, strict: false)
+
+    # Overload inspect for easier to read output
+    def inspect : String
+      "<Amnesia::Email @id=#{id} @from=\"#{from}\" @subject=\"#{subject}\">"
+    end
+  end
 end
